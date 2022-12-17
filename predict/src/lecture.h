e266c35dd2efaf8d155063a48a068d1251f88ba0
defouni <nicolas-defour@ensta-bretagne.org>
2022-12-17 11:19:15 +0100

    wip: changement de train donc sauvegarde (debut pred)

---
+++ predict/src/lecture.h
@@ -0,0 +1,7 @@
+#ifndef LECTURE__H
+#define LECTURE_H
+#include <string>
+
+void extraction_descripteur(string file_name, double mu[], double sigma[]);
+
+#endif
---
+++ predict/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "code.h"
+#include "lecture.h"
+
+using namespace std;
+
+int main(){
+    
+}
