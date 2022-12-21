f64b474d9fdbdb2983db8b1696dbde167107a0f3
defouni <nicolas-defour@ensta-bretagne.org>
2022-12-21 21:23:29 +0100

    traitement SVC

--- predict/script/main.py
+++ predict/script/main.py
@@ -5,22 +5,31 @@ with open('../model/model_svm.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile)
     i = 0
     data = np.zeros((10,1024))
+    means = np.zeros((1024, 1))
+    scales =  np.zeros((1024, 1))
     iter = np.zeros(10)
     for row in spamreader:
-        if(i<10):
+        if(i==0):
             for j in range(1024):
-                # print(row[j])
+                means[j, 0] = row[j]
+        elif(i==1):
+            for j in range(1024):
+                scales[j, 0] = row[j]
+        elif(i<10):
+            for j in range(1024):
                 data[i, j] = row[j]
         else:
             for j in range(10):
-                # print(row[j])
                 iter[j] = row[j]
         i += 1
 
 with open('descripteur.csv', newline='') as csvfile:
     x = list(csv.reader(csvfile))
-x = np.array(x)
-print((x.T).shape)
-print(data.shape)
-c = data@x+iter;
-print(c)
+
+a = np.zeros((1024, 1))
+a = (a-means)/scales
+for i in range(1024):
+    a[i] = float(x[0][i])
+c = np.dot(data, a).T+ iter;
+print((c))
+print(np.max(c))
